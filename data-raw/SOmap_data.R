326b1bb5ad4a3481b88f0f8beac7047db74967c4
Ben Raymond <ben.raymond@aad.gov.au>
2018-10-18 09:07:45 +1100

    get EEZ data from CCAMLR

--- data-raw/SOmap_data.R
+++ data-raw/SOmap_data.R
@@ -65,15 +65,18 @@ if (any(chk)) stop("non-ASCII chars in continent data")
 
 ## fronts (was ocean1)
 fronts_orsi <- spTransform(orsifronts::orsifronts, CRS(psproj))
-
 chk <- sapply(names(fronts_orsi), function(z) length(tools::showNonASCII(fronts_orsi[[z]])) > 0)
 if (any(chk)) stop("non-ASCII chars in fronts_orsi data")
 
-
 ## eez and eez_coast (was EEZ1)
-#devtools::install_github('SymbolixAU/geojsonsf')
-library(geojsonsf)
-eezlist <- lapply(c(25513, 8383, 8385, 8388, 8387, 8384, 8399), function(id) {
+files <- get_ccamlr_data("https://data.ccamlr.org/sites/default/files/eez-shapefile-WGS84.zip")
+EEZ1 <- spTransform(raster::shapefile(files[grepl("shp$", files)]), CRS(psproj))
+
+if (FALSE) {
+    ## or to derive from the original files from marineregions.org
+    ##devtools::install_github('SymbolixAU/geojsonsf')
+    library(geojsonsf)
+    eezlist <- lapply(c(25513, 8383, 8385, 8388, 8387, 8384, 8399), function(id) {
   key <- if (id %in% c(25513)) "eez_iho" else "eez"
   this_url <- paste0("http://geo.vliz.be/geoserver/wfs?request=getfeature&service=wfs&version=1.1.0&typename=MarineRegions:", key, "&outputformat=json&filter=%3CPropertyIsEqualTo%3E%3CPropertyName%3Emrgid%3C%2FPropertyName%3E%3CLiteral%3E", id, "%3C%2FLiteral%3E%3C%2FPropertyIsEqualTo%3E") ## geojson
 
@@ -82,30 +85,25 @@ eezlist <- lapply(c(25513, 8383, 8385, 8388, 8387, 8384, 8399), function(id) {
   geojson_sf(tf)
 })
 
-common <- purrr::reduce(purrr::map(eezlist, names), intersect)
-eez_coast <- st_geometry(sf::st_transform(do.call(rbind, purrr::map(eezlist, ~.x[common])), psproj))
+    common <- purrr::reduce(purrr::map(eezlist, names), intersect)
+    eez_coast <- st_geometry(sf::st_transform(do.call(rbind, purrr::map(eezlist, ~.x[common])), psproj))
 
-## kill the coast
-library(dplyr)
-eez <- st_cast(eez_coast, "POLYGON")
-#g <- st_geometry(eez)
-#eez <- st_set_geometry(eez, sf::st_sfc(lapply(g, function(x) sf::st_polygon(x[1])), crs = st_crs(g))) %>% dplyr::select(territory1, everything())
-eez <- sf::st_sfc(lapply(eez, function(x) sf::st_polygon(x[1])), crs = st_crs(eez))
+    ## kill the coast
+    library(dplyr)
+    eez <- st_cast(eez_coast, "POLYGON")
+    ##g <- st_geometry(eez)
+    ##eez <- st_set_geometry(eez, sf::st_sfc(lapply(g, function(x) sf::st_polygon(x[1])), crs = st_crs(g))) %>% dplyr::select(territory1, everything())
+    eez <- sf::st_sfc(lapply(eez, function(x) sf::st_polygon(x[1])), crs = st_crs(eez))
+    eez_coast <- as(eez_coast, "Spatial")
+    EEZ1 <- as(eez, "Spatial")
+}
 
-
-
-
-eez_coast <- as(eez_coast, "Spatial")
-EEZ1 <- as(eez, "Spatial")
-
-
-fronts_orsi <- spTransform(orsifronts::orsifronts, CRS(psproj))
 
 SOmap_data <- list(CCAMLR_MPA = MPA1, CCAMLR_statistical_areas = CCAMLR1, CCAMLR_research_blocks = RB1,
                    CCAMLR_SSRU = SSRU1, CCAMLR_SSMU = SSMU1,
                   # continent = continent, fronts_orsi = fronts_orsi,
-                  eez_coast = eez_coast)
+                  EEZ = EEZ1)
 
 devtools::use_data(SOmap_data, overwrite = TRUE, compress = "xz")
 
-devtools::use_data(EEZ1, CCAMLR1, continent, fronts_orsi, MPA1, RB1, SSMU1, SSRU1, compress = "xz")
+devtools::use_data(EEZ1, CCAMLR1, continent, fronts_orsi, MPA1, RB1, SSMU1, SSRU1, compress = "xz", overwrite = TRUE)
