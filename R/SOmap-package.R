64709d66f82b6b4f4ef200bc942b2d938789b732
Ben Raymond <ben.raymond@aad.gov.au>
2018-10-18 15:24:12 +1100

    change package-level doc to avoid alias clash

--- R/SOmap-package.R
+++ R/SOmap-package.R
@@ -1,3 +1,9 @@
-#' @keywords internal
+#' \pkg{SOmap}
+#'
+#' Create publication-quality Southern Ocean maps in a simple manner with multiple management layer options.
+#'
+#' @name SOmap-package
+#' @docType package
 #' @importFrom utils data head
-"_PACKAGE"
+NULL
+
